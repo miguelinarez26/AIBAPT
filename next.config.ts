f02801a39f640f15c072b1f3a06744eda96ab318
Carolina Ustariz <caroust@gmail.com>
2026-06-16 00:02:53 -0400

    listo

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
-
 const nextConfig: NextConfig = {
-  basePath,
-  assetPrefix: basePath || undefined,
   images: {
     unoptimized: true,
     remotePatterns: [
