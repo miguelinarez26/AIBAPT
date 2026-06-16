2c9dfd36dedbb07c801da1f7ede24033b33f192e
Carolina Ustariz <caroust@gmail.com>
2026-06-15 22:15:56 -0400

    ahora si boludo

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -4,6 +4,9 @@ import Footer from "@/components/Footer";
 import { LanguageProvider } from "@/contexts/LanguageContext";
 import { AuthProvider } from "@/components/providers/AuthProvider";
 
+export function generateStaticParams() {
+  return [{ lang: 'es' }, { lang: 'pt' }];
+}
 export default function LocalizedLayout({
   children,
 }: Readonly<{
